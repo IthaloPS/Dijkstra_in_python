fd9d67445ee65777d84e2dfd12e742e96055b905
Gabriel Zacarias <gazacharias2002@gmail.com>
2024-03-28 23:22:49 -0300

    Update dijkstra.py

--- dijkstra.py
+++ dijkstra.py
@@ -1,13 +1,11 @@
 import sys
 
+
 def dijkstra(graph, origin, destination):
-    if(origin not in graph or destination not in graph):
-        return 'Valores não existentes!!!'
-    
     distance = {v: sys.maxsize for v in graph}
     distance[origin] = 0
     visited = set()
-    
+
     while visited != set(distance):
         current_vertex = None
         short_distance = sys.maxsize
@@ -16,8 +14,8 @@ def dijkstra(graph, origin, destination):
             if v not in visited and distance[v] < short_distance:
                 current_vertex = v
                 short_distance = distance[v]
-                if (v == destination):
-                  break
+                if v == destination:
+                    break
 
         visited.add(current_vertex)
 
@@ -27,6 +25,7 @@ def dijkstra(graph, origin, destination):
 
     return distance
 
+
 graph = {
     'ARAD': {'ZERIND': 75, 'SIBIU': 140, 'TIMISOARA': 118},
     'ZERIND': {'ARAD': 75, 'ORADEA': 71},
@@ -35,11 +34,11 @@ graph = {
     'TIMISOARA': {'ARAD': 118, 'LUGOJ': 111},
     'LUGOJ': {'TIMISOARA': 111, 'MEHADIA': 70},
     'MEHADIA': {'LUGOJ': 70, 'DROBETA': 75},
-    'DROBETA': {'MEHADIA': 75, ' CRAIOVA': 120},
-    ' CRAIOVA': {'DROBETA': 120, 'RIMNICU VILCEA': 146, 'PITESTI': 138},
-    'RIMNICU VILCEA': {'SIBIU': 80, ' CRAIOVA': 146, 'PITESTI': 97},
+    'DROBETA': {'MEHADIA': 75, 'CRAIOVA': 120},
+    'CRAIOVA': {'DROBETA': 120, 'RIMNICU VILCEA': 146, 'PITESTI': 138},
+    'RIMNICU VILCEA': {'SIBIU': 80, 'CRAIOVA': 146, 'PITESTI': 97},
     'FAGARAS': {'SIBIU': 99, 'BUCHAREST': 211},
-    'PITESTI': {'RIMNICU VILCEA': 97, ' CRAIOVA': 138, 'BUCHAREST': 101},
+    'PITESTI': {'RIMNICU VILCEA': 97, 'CRAIOVA': 138, 'BUCHAREST': 101},
     'BUCHAREST': {'FAGARAS': 211, 'PITESTI': 101, 'GIURGIU': 90, 'URZICENI': 85},
     'GIURGIU': {'BUCHAREST': 90},
     'URZICENI': {'BUCHAREST': 85, 'VASLUI': 142, 'HIRSOVA': 98},
@@ -50,8 +49,8 @@ graph = {
     'EFORIE': {'HIRSOVA': 86}
 }
 
-origin = str(input('from: ').upper())
-destination = str(input('to: ').upper())
+origin = str(input('from: ').upper().rstrip().lstrip())
+destination = str(input('to: ').upper().rstrip().lstrip())
 if origin not in graph or destination not in graph:
     print('Valores não existentes!!!')
 else:
