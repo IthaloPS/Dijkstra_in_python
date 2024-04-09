1aa7dcb9677b0bfe3d9e13e1fff7615a3b9d13df
ithalo <ithalo@Kali.kaliithalo>
2024-04-09 19:37:03 -0300

    fix small issues

--- dijkstra.py
+++ dijkstra.py
@@ -37,18 +37,17 @@ def visualizar_grafo(grafo, caminho, origem, destino):
 
     pos = nx.spring_layout(G)
 
-    nx.draw_networkx_nodes(G, pos, node_color='lightblue', node_size=700)
-    nx.draw_networkx_edges(G, pos, width=1.0, alpha=0.5)
+    nx.draw_networkx_nodes(G, pos, node_color='lightblue', node_size=600)
+    nx.draw_networkx_edges(G, pos, width=1.0, alpha=1)
 
     caminho_arestas = [(caminho[i], caminho[i + 1]) for i in range(len(caminho) - 1)]
     nx.draw_networkx_edges(G, pos, edgelist=caminho_arestas, edge_color='red', width=2.0)
 
-    nx.draw_networkx_nodes(G, pos, nodelist=[origem, destino], node_color='green', node_size=700)
+    nx.draw_networkx_nodes(G, pos, nodelist=[origem, destino], node_color='green', node_size=600)
 
-    nx.draw_networkx_labels(G, pos, font_size=7)
-
+    nx.draw_networkx_labels(G, pos, font_size=6)
     edge_labels = {(u, v): d['weight'] for u, v, d in G.edges(data=True)}
-    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=6)
 
     plt.title("Grafo com Caminho Percorrido")
     plt.axis('off')
@@ -76,17 +75,17 @@ grafo_cidade_base = {
     'HIRSOVA': {'URZICENI': 98, 'EFORIE': 86},
     'EFORIE': {'HIRSOVA': 86}
 }
+while True:
+    origem_entrada = str(input('Saindo de: ').upper().rstrip().lstrip())
+    destino_entrada = str(input('Indo para: ').upper().rstrip().lstrip())
 
-origem_entrada = str(input('Saindo de: ').upper().rstrip().lstrip())
-destino_entrada = str(input('Indo para: ').upper().rstrip().lstrip())
-
-if origem_entrada not in grafo_cidade_base or destino_entrada not in grafo_cidade_base:
-    print('Valores não existentes!!!')
-elif origem_entrada == destino_entrada:
-    print(f'Você já está em {destino_entrada}!')
-else:
-    distancia_total, caminho = dijkstra(grafo_cidade_base, origem_entrada, destino_entrada)
-    print(f'O caminho mais curto de {origem_entrada} até {destino_entrada} é de {distancia_total} KM')
-    print('Caminho percorrido:')
-    print(' -> '.join(caminho))
-    visualizar_grafo(grafo_cidade_base, caminho, origem_entrada, destino_entrada)
+    if origem_entrada not in grafo_cidade_base or destino_entrada not in grafo_cidade_base:
+        print('Valores não existentes!!!')
+    elif origem_entrada == destino_entrada:
+        print(f'Você já está em {destino_entrada}!')
+    else:
+        distancia_total, caminho = dijkstra(grafo_cidade_base, origem_entrada, destino_entrada)
+        print(f'O caminho mais curto de {origem_entrada} até {destino_entrada} é de {distancia_total} KM')
+        print('Caminho percorrido:')
+        print(' -> '.join(caminho))
+        visualizar_grafo(grafo_cidade_base, caminho, origem_entrada, destino_entrada)
